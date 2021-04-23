dc4adfae7d4464f19bc76da4cd7ee113eea1b850
Àlex Castelló <ac.desenvolupador@gmail.com>
2021-04-23 23:18:51 +0200

    Nivell 3

--- js/exercici04.js
+++ js/exercici04.js
@@ -1,19 +1,21 @@
 "use strict";
-const mot="alexander";
-const motRix=mot.toUpperCase().split("");
-const mapMot=new Map();
+const mot = "alexander";
+const motRix = mot.toUpperCase().split("");
+const mapMot = new Map();
 
 
 /* EXERCICI 01 */
 function exercici01() {
-    motRix.forEach(e=>{ console.log(e); });
+    motRix.forEach(e => {
+        console.log(e);
+    });
 }
 //exercici01();
 
 /* EXERCICI 02 */
 function exercici02() {
     let vocals = "aeiou"
-    motRix.forEach(e=>{
+    motRix.forEach(e => {
         // Esbrinar si és un número
         if (/[0123456789]/.test(e)) {
             console.log("Els noms no contenen números");
@@ -21,13 +23,13 @@ function exercici02() {
 
         // Esbrinar si aquell caracter és vocal
         if (/[AEIOU]/.test(e)) {
-            console.log("He trobat la vocal: "+e);
+            console.log("He trobat la vocal: " + e);
         }
 
         // Esbrinar si aquell caracter és consonant
         // Esbrinar si aquell caracter és vocal
         if (/[BCDFGHJKLMNPQRSTVWXYZ]/.test(e)) {
-            console.log("He trobat la consonant: "+e);
+            console.log("He trobat la consonant: " + e);
         }
     });
 }
@@ -35,17 +37,19 @@ function exercici02() {
 
 /* EXERCICI 03 */
 function compta(p) {
-    let c=0;
+    let c = 0;
 
-    for(let i=0;i<motRix.length;i++) {
-        if(motRix[i]==p) { c++; }
+    for (let i = 0; i < motRix.length; i++) {
+        if (motRix[i] == p) {
+            c++;
+        }
     }
     return c;
 }
 
 function exercici03() {
-    motRix.forEach(e=>{
-        mapMot.set(e,compta(e));
+    motRix.forEach(e => {
+        mapMot.set(e, compta(e));
     });
 
     console.log(mapMot);
@@ -54,10 +58,13 @@ function exercici03() {
 //exercici03();
 
 /* EXERCICI 04 */
-let name="alexander";
-let surname="castello";
-let nameArray=name.split("");
-nameArray.push(" ");
-let surnameArray=surname.split("");
-let fullName=nameArray.concat(surnameArray);
-console.log(fullName);
+function exercici04() {
+    let name = "alexander";
+    let surname = "castello";
+    let nameArray = name.split("");
+    nameArray.push(" ");
+    let surnameArray = surname.split("");
+    let fullName = nameArray.concat(surnameArray);
+    console.log(fullName);
+}
+exercici04();
